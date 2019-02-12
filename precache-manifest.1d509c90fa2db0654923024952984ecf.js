4982c517eac2ec4e38251a222a02983ec9a1e693
gitmatheo <matteuszd@gmail.com>
2019-02-12 08:20:43 +0100

    Updates

---
+++ precache-manifest.1d509c90fa2db0654923024952984ecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f07c28da8158bd964d8",
+    "url": "/React-contact-manager/static/js/runtime~main.6f07c28d.js"
+  },
+  {
+    "revision": "ebbe4a29185656b22c3f",
+    "url": "/React-contact-manager/static/js/main.ebbe4a29.chunk.js"
+  },
+  {
+    "revision": "b9edcad17803749dc5f7",
+    "url": "/React-contact-manager/static/js/2.b9edcad1.chunk.js"
+  },
+  {
+    "revision": "b9edcad17803749dc5f7",
+    "url": "/React-contact-manager/static/css/2.fa4dd012.chunk.css"
+  },
+  {
+    "revision": "75228ec2a8c7f72659599e7b9634fe3a",
+    "url": "/React-contact-manager/index.html"
+  }
+];
